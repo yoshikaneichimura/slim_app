9efb29939d487d2006441feff9799bec0c110d37
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-01-01 13:25:29 +0000

    model routing controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
