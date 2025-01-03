4ae021e91244654df2ba9bb48f1d0393a8cbc963
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-01-03 21:43:32 +0000

    [Add] CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
   root to: 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
